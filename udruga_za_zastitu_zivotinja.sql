1ebcd15ffa9b2e4bfd3a46cfd1bf655cce7769f8
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-05-16 13:17:39 +0200

    Update udruga_za_zastitu_zivotinja.sql

--- udruga_za_zastitu_zivotinja.sql
+++ udruga_za_zastitu_zivotinja.sql
@@ -25,3 +25,26 @@ create table prostor(
     broj int not null,
     zivotinja int
 );
+
+# kreiranje veza
+alter table prostor add foreign key (zivotinja)
+references zivotinja (sifra);
+
+alter table zivotinja add foreign key (osoba)
+references osoba (sifra);
+
+# dodavanje podataka u bazu
+insert into osoba (sifra,ime,prezime,oib)
+values (null,'Pero','Perić',123456789),
+		(null,'Marko','Marić',456789123),
+		(null,'Ivan','Ivić',789123456);
+
+insert into zivotinja (sifra,ime,vrsta,osoba)
+values (null,'Medo','pas',1),
+		(null,'Micko','mačka',2),
+		(null,'Garo','pas',1);
+
+insert into prostor (sifra,broj,zivotinja)
+values (null,1,1),
+		(null,2,2),
+		(null,5,3);
